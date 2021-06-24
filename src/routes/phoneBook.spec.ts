d1ccefd98e4d02bae2d16977e0610eadef1d80fa
Emiliano <emilianomarchiol@gmail.com>
2021-06-24 13:33:24 +0100

    unit tests update
    - adds unit tests for other endpoints.
    - updates code  with try-catch.

--- src/routes/phoneBook.spec.ts
+++ src/routes/phoneBook.spec.ts
@@ -1,28 +1,33 @@
 jest.mock("../sql/contacts-db", () => ({
-    getAllContacts: jest.fn(() => [])
+    getAllContacts: jest.fn(() => []),
+    addOrUpdateContact: jest.fn(() => []),
+    deleteContactById: jest.fn(()=>{})
 }));
-import { getAllContacts } from "../sql/contacts-db";
-import { list } from "./phoneBook";
+import { getAllContacts, addOrUpdateContact, deleteContactById } from "../sql/contacts-db";
+import { list, addOrUpdate, deleteById } from "./phoneBook";
 
 describe("phone book routes", () => {
-    describe("list", () => {
-        let requestMock: any;
-        let responseMock: any;
+    let requestMock: any;
+    let responseMock: any;
 
-        beforeEach(() => {
-            requestMock = {};
-            responseMock = {
-                viewName: "",
-                data: {},
-                status: (status: number) => {
-                    return {
-                        json: (value: any) => { return value }
-                    }
+    beforeEach(() => {
+        requestMock = {};
+        responseMock = {
+            viewName: "",
+            data: {},
+            status: (status: number) => {
+                return {
+                    code: status,
+                    json: (value: any) => { return value }
                 }
-            };
-        });
+            }
+        };
+    });
+
+    describe("list", () => {
+
         it("should call getAllContacts", async () => {
-            const result = await list(requestMock, responseMock);
+            await list(requestMock, responseMock);
 
             expect(getAllContacts).toHaveBeenCalledTimes(1);
         });
@@ -49,6 +54,67 @@ describe("phone book routes", () => {
             const result = await list(requestMock, responseMock);
 
             expect(result).toEqual({ error: "Error while retrieving data" });
+        });
+    });
+
+    describe("addOrUpdate", () => {
+        it("should call addOrUpdateContact", async () => {
+            await addOrUpdate(requestMock, responseMock);
+
+            expect(addOrUpdateContact).toHaveBeenCalledTimes(1);
+        });
+
+        it("should return 200 if the amount of affected rows is > 0 from addOrUpdate", async () => {
+            (addOrUpdateContact as jest.Mock).mockImplementation(() => Promise.resolve(1));
+            const result = await addOrUpdate(requestMock, responseMock);
+
+            expect(result.code).toEqual(200);
+        });
+
+        it("should return an error if the amount of affected rows is equal 0 from addOrUpdate", async () => {
+            (addOrUpdateContact as jest.Mock).mockImplementation(() => Promise.resolve(0));
+            const result = await addOrUpdate(requestMock, responseMock);
+
+            expect(result).toEqual({ error: "Conflict when adding or updating new contact" });
+        });
+
+        it("should return 500 if addOrUpdateContact throws an errorr", async () => {
+            (addOrUpdateContact as jest.Mock).mockImplementation(() => {throw("Invalid username and password for db")});
+            const result = await addOrUpdate(requestMock, responseMock);
+
+            expect(result).toEqual({ error: "Error while retrieving data" });
+        });
+    });
+
+    describe("deleteById", () => {
+        beforeEach(()=>{
+            requestMock = {params: {id: 1}};
+        })
+        it("should call deleteContactById", async () => {
+            await deleteById(requestMock, responseMock);
+            
+            expect(deleteContactById).toHaveBeenCalledTimes(1);
+        });
+
+        it("should return 200 if the amount of affected rows is > 0 from deleteById", async () => {
+            (deleteContactById as jest.Mock).mockImplementation(() => Promise.resolve(1));
+            const result = await deleteById(requestMock, responseMock);
+
+            expect(result.code).toEqual(200);
+        });
+
+        it("should return 204 if the amount of affected rows is = 0 from deleteById", async () => {
+            (deleteContactById as jest.Mock).mockImplementation(() => Promise.resolve(0));
+            const result = await deleteById(requestMock, responseMock);
+
+            expect(result.code).toEqual(204);
+        });
+
+        it("should return 500 if deleteContactById throws an errorr", async () => {
+            (deleteContactById as jest.Mock).mockImplementation(() => {throw("Invalid username and password for db")});
+            const result = await deleteById(requestMock, responseMock);
+
+            expect(result).toEqual({ error: "Error while deleting record" });
         });
     });
 });
--- src/routes/phoneBook.ts
+++ src/routes/phoneBook.ts
@@ -2,39 +2,35 @@ import { Contact } from "../interfaces/contact";
 import { getAllContacts, deleteContactById, addOrUpdateContact } from "../sql/contacts-db";
 
 // Get
-export async function list(req: any, res: any): Promise<any[]> {
+export async function list(req: any, res: any): Promise<Contact[]> {
     try {
         const contacts: Contact[] = await getAllContacts();
         return res.status(200).json(contacts);
     }
-    catch(err) {
-        return res.status(500).json({error: "Error while retrieving data"});
+    catch (err) {
+        return res.status(500).json({ error: "Error while retrieving data" });
     }
 };
 
 // Post
-export async function addOrUpdate(req: any, res: any): Promise<any[]> {
-    const record: number = await addOrUpdateContact(req.body, req.params);
-    
-    return record > 0 ? res.status(200) : 
-    res.status(404).json({error: "data not found"});
-};
+export async function addOrUpdate(req: any, res: any): Promise<any> {
+    try {
+        const record: number = await addOrUpdateContact(req.body, req.params);
 
-// Post
-export async function updateById(req: any, res: any): Promise<any[]> {
-    const contacts: Contact[] = await getAllContacts();
-    console.log("contacts from db:", contacts);
-    return contacts ? res.status(200).json(contacts) : 
-    res.status(404).json({error: "data not found"});
+        return record > 0 ? res.status(200) :
+            res.status(409).json({ error: "Conflict when adding or updating new contact" });
+    } catch (err) {
+        return res.status(500).json({ error: "Error while retrieving data" });
+    }
 };
 
 // Delete
-export async function deleteById(req: any, res: any): Promise<any[]> {
+export async function deleteById(req: any, res: any): Promise<any> {
     try {
-        await deleteContactById(req.params.id);
-        return res.status(200);
+        const record: number = await deleteContactById(req.params.id);
+        return record > 0 ? res.status(200) : res.status(204);
     }
-    catch(err) {
-        return res.status(500).json({error: "Error while deleting record"});
+    catch (err) {
+        return res.status(500).json({ error: "Error while deleting record" });
     }
 };
